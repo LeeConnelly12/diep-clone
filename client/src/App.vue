af77c0899441f2072933666e262e463095b00510
Lee Connelly <lee@leeconnelly.dev>
2022-12-14 23:13:41 +0000

    Create join game form

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,65 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const canvas = ref(null)
+
+const form = ref({
+  name: null,
+})
+
+const isPlaying = ref(false)
+
+const players = ref([])
+
+onMounted(() => {
+  const ctx = canvas.value.getContext('2d')
+
+  canvas.value.width = window.innerWidth
+  canvas.value.height = window.innerHeight
+
+  ctx.fillStyle = '#CDCDCD'
+  ctx.fillRect(0, 0, canvas.value.width, canvas.value.height)
+
+  ctx.lineWidth = 2
+  ctx.strokeStyle = '#C9C9C9'
+
+  for (let x = 0; x <= canvas.value.width; x += 30) {
+    ctx.moveTo(x, 0)
+    ctx.lineTo(x, canvas.value.height)
+  }
+
+  for (let y = 0; y <= canvas.value.height; y += 30) {
+    ctx.moveTo(0, y)
+    ctx.lineTo(canvas.value.width, y)
+  }
+
+  ctx.stroke()
+})
+
+const submit = async (e) => {
+  console.log('submitted')
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="relative grid place-items-center">
+    <canvas ref="canvas"></canvas>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <form @submit.prevent="submit" class="absolute">
+      <p class="text-shadow text-center text-2xl text-white">This is the tale of...</p>
+      <div class="mt-1 flex gap-2">
+        <input v-model="form.name" type="text" class="h-14 w-80 border-4 border-black bg-white text-5xl" />
+        <button class="text-shadow h-14 w-24 border-4 border-[#555555] bg-[#8EFFFB] text-2xl text-white">Play</button>
+      </div>
+    </form>
+
+    <aside v-if="isPlaying" class="absolute top-8 right-8">
+      <h2>Scoreboard</h2>
+      <ul>
+        <li v-for="player in players" :key="player.id">
+          {{ player.name }}
+        </li>
+      </ul>
+    </aside>
+  </main>
+</template>
