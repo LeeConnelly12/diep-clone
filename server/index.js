26a1b4021f28efe77a2624855db35f1a1e2b0a82
Lee Connelly <lee@leeconnelly.dev>
2022-12-20 02:39:56 +0000

    Use HTTPS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import Websocket, { WebSocketServer } from 'ws'
-import { createServer } from 'http'
+import { createServer } from 'https'
 import fs from 'fs'
 
 dotenv.config()
